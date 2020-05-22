cc5cfb0ffeb6799d0d6f3c900a428e30a40963b6
deniallugo <l.daniil@codewizard.team>
2020-05-22 16:34:17 +0300

    feat(tests): Add one test for dispatcher

--- schloss/types.py
+++ schloss/types.py
@@ -1,6 +1,6 @@
 from typing import Awaitable, Callable, Union
 
-from schloss import SchlossSession
+from schloss.session import SchlossSession
 
 Message = object  # Protobuf message or something simillar
 KafkaHandler = Callable[[SchlossSession], Union[Awaitable, None]]
